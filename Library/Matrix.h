32585f5c199a5acc94f63af76651b110c39bd380
seeuagony <163781660+seeuagony@users.noreply.github.com>
2025-10-23 04:36:40 +0300

    Update Matrix.h

--- Library/Matrix.h
+++ Library/Matrix.h
@@ -4,3 +4,42 @@
 #include <string>
 #include <sstream>
 #include <stdexcept>
+
+namespace miit::algebra {
+    /**
+    @brief Класс динамического массива шаблон 
+    @tparam T тип элемента массива
+    */
+    template<typename T>
+    class Matrix {
+        private:
+        T* data;
+        size_t size;
+
+        public:
+        /**
+        @brief  конструктор по умолчанию
+        */
+        Matrix();
+
+        /**
+        @brief Конструктор с параметрами размера 
+        */
+        explicit Matrix(size_t n);
+
+        /**
+        @brief Копирующий конструктор 
+        */
+        Matrix(const Matrix& other);
+
+        /**
+        @brief Переносящий конструктор 
+        */
+        Matrix(const Matrix&& other) noexcept;
+
+        /**
+        @brief Деструктор
+        */
+        ~Matrix();
+    }
+}
