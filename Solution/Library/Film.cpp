a325138ece2b28e85e4caf40dad379d793dc7296
seeuagony <163781660+seeuagony@users.noreply.github.com>
2025-12-18 02:14:05 +0300

    перемены короче
    новая реализация film.h film.cpp

--- Solution/Library/Film.cpp
+++ Solution/Library/Film.cpp
@@ -1,116 +1,119 @@
 #include "Film.h"
-#include <sstream>
-#include <algorithm>
+#include "MediaType.h"
 
-namespace miit::cinema
-{
-    Film::Film(const std::string& title, const Person& director, const std::string& description, double price) : title(title), director(director), description(description), price(price)
-    {
+namespace miit::cinema {
+
+Film::Film() : title(""), genre(""), year(0), director(""), description(""), actors(), available_media() {}
+
+Film::Film(const std::string& title, const std::string& genre, int year, 
+           const std::string& director, const std::string& description)
+    : title(title), genre(genre), year(year), director(director), 
+      description(description), actors(), available_media() {}
+
+Film::Film(const Film& other)
+    : title(other.title), genre(other.genre), year(other.year),
+      director(other.director), description(other.description),
+      actors(other.actors), available_media(other.available_media) {}
+
+Film& Film::operator=(const Film& other) {
+    if (this != &other) {
+        title = other.title;
+        genre = other.genre;
+        year = other.year;
+        director = other.director;
+        description = other.description;
+        actors = other.actors;
+        available_media = other.available_media;
     }
+    return *this;
+}
 
-    std::string Film::getTitle() const
-    {
-        return title;
+std::string Film::get_title() const {
+    return title;
+}
+
+std::string Film::get_genre() const {
+    return genre;
+}
+
+int Film::get_year() const {
+    return year;
+}
+
+std::string Film::get_director() const {
+    return director;
+}
+
+std::string Film::get_description() const {
+    return description;
+}
+
+const std::vector<std::string>& Film::get_actors() const {
+    return actors;
+}
+
+const std::vector<MediaType*>& Film::get_available_media() const {
+    return available_media;
+}
+
+void Film::add_actor(const std::string& actor) {
+    actors.push_back(actor);
+}
+
+void Film::add_media(MediaType* media) {
+    if (media == nullptr)
+        return;
+    for (MediaType* existing_media : available_media) {
+        if (existing_media == media)
+            return;
     }
+    available_media.push_back(media);
+}
 
-    Person Film::getDirector() const
-    {
-        return director;
-    }
-
-    std::string Film::getDescription() const
-    {
-        return description;
-    }
-
-    double Film::getPrice() const
-    {
-        return price;
-    }
-
-    std::vector<Genre> Film::getGenres() const
-    {
-        return genres;
-    }
-
-    std::vector<Person> Film::getCast() const
-    {
-        return cast;
-    }
-
-    std::vector<Sale> Film::getSales() const
-    {
-        return sales;
-    }
-
-    void Film::addGenre(const Genre& genre)
-    {
-        genres.push_back(genre);
-    }
-
-    void Film::addActor(const Person& actor)
-    {
-        cast.push_back(actor);
-    }
-
-    void Film::addSale(const Sale& sale)
-    {
-        sales.push_back(sale);
-    }
-
-    int Film::getTotalSales() const
-    {
-        int total = 0;
-        for (const auto& sale : sales) {
-            total += sale.getQuantity();
+std::ostream& operator<<(std::ostream& os, const Film& film) {
+    os << "Film" << std::endl;
+    os << "Title: " << film.title << std::endl;
+    os << "Genre: " << film.genre << std::endl;
+    os << "Year: " << film.year << std::endl;
+    os << "Director: " << film.director << std::endl;
+    os << "Description: " << film.description << std::endl;
+    
+    os << "Actors: ";
+    if (film.actors.empty()) {
+        os << "no actors listed" << std::endl;
+    } else {
+        os << std::endl;
+        for (size_t i = 0; i < film.actors.size(); ++i) {
+            os << "  - " << film.actors[i] << std::endl;
         }
-        return total;
     }
+    
+    os << "Available media: " << film.available_media.size() << " type(s)" << std::endl;
+    
+    return os;
+}
 
-    double Film::getTotalRevenue() const
-    {
-        return getTotalSales() * price;
-    }
+std::istream& operator>>(std::istream& is, Film& film) {
+    std::cout << "Enter title: ";
+    std::getline(is, film.title);
+    
+    std::cout << "Enter genre: ";
+    std::getline(is, film.genre);
+    
+    std::cout << "Enter year: ";
+    is >> film.year;
+    is.ignore();
+    
+    std::cout << "Enter director: ";
+    std::getline(is, film.director);
+    
+    std::cout << "Enter description: ";
+    std::getline(is, film.description);
+    
+    film.actors.clear();
+    film.available_media.clear();
+    
+    return is;
+}
 
-    bool Film::hasGenre(const Genre& genre) const
-    {
-        return std::find(genres.begin(), genres.end(), genre) != genres.end();
-    }
-
-    bool Film::hasActor(const Person& actor) const
-    {
-        return std::find(cast.begin(), cast.end(), actor) != cast.end();
-    }
-
-    std::string Film::getInfo() const
-    {
-        std::ostringstream oss;
-        oss << "Название: " << title << "\n";
-        oss << "Тип носителя: " << getMediaType() << "\n";
-        oss << "Режиссёр: " << director.getFullName() << "\n";
-        
-        oss << "Жанры: ";
-        for (size_t i = 0; i < genres.size(); i++) {
-            oss << genres[i].getName();
-            if (i < genres.size() - 1) {
-                oss << ", ";
-            }
-        }
-        oss << "\n";
-        
-        oss << "Актёры: ";
-        for (size_t i = 0; i < cast.size(); i++) {
-            oss << cast[i].getFullName();
-            if (i < cast.size() - 1) {
-                oss << ", ";
-            }
-        }
-        oss << "\n";
-        
-        oss << "Описание: " << description << "\n";
-        oss << "Цена: " << price << " руб.\n";
-        oss << "Продано копий: " << getTotalSales() << "\n";
-        
-        return oss.str();
-    }
 }
--- Solution/Library/Film.h
+++ Solution/Library/Film.h
@@ -1,138 +1,130 @@
 #pragma once
-
-#include "Genre.h"
-#include "Person.h"
-#include "Sale.h"
 #include <string>
 #include <vector>
+#include <iostream>
 
-namespace miit::cinema
-{
+namespace miit::cinema {
+
+class MediaType;
+
+/**
+ * @brief класс, описывающий фильм
+ */
+class Film {
+private:
+    std::string title;
+    std::string genre;          
+    int year;    
+    std::string director;
+    std::string description;
+    
+    std::vector<std::string> actors;
+    std::vector<MediaType*> available_media;
+
+public:
     /**
-     * @brief Базовый абстрактный класс для всех типов фильмов
+     * @brief конструктор по умолчанию
      */
-    class Film
-    {
-    protected:
-        std::string title;
-        std::vector<Genre> genres;
-        Person director;
-        std::vector<Person> cast;
-        std::string description;
-        double price;
-        std::vector<Sale> sales;
+    Film();
+    
+    /**
+     * @brief конструктор с параметрами
+     * @param title название фильма
+     * @param genre жанр фильма
+     * @param year год выпуска
+     * @param director режиссёр
+     * @param description краткое содержание
+     */
+    Film(const std::string& title, const std::string& genre, int year, 
+         const std::string& director, const std::string& description);
+    
+    /**
+     * @brief конструктор копирования
+     * @param other другой объект класса Film
+     */
+    Film(const Film& other);
+    
+    /**
+     * @brief перегрузка оператора присваивания
+     * @param other другой объект класса Film
+     * @return ссылка на текущий объект
+     */
+    Film& operator=(const Film& other);
+    
+    /**
+     * @brief деструктор по умолчанию
+     */
+    ~Film() = default;
+    
+    /**
+     * @brief возвращает название фильма
+     * @return название фильма
+     */
+    std::string get_title() const;
+    
+    /**
+     * @brief возвращает жанр фильма
+     * @return жанр фильма
+     */
+    std::string get_genre() const;
+    
+    /**
+     * @brief возвращает год выпуска
+     * @return год выпуска
+     */
+    int get_year() const;
+    
+    /**
+     * @brief возвращает режиссёра
+     * @return режиссёр
+     */
+    std::string get_director() const;
+    
+    /**
+     * @brief возвращает краткое содержание
+     * @return описание фильма
+     */
+    std::string get_description() const;
+    
+    /**
+     * @brief возвращает список актёров
+     * @return константная ссылка на вектор актёров
+     */
+    const std::vector<std::string>& get_actors() const;
+    
+    /**
+     * @brief возвращает список доступных носителей
+     * @return константная ссылка на вектор носителей
+     */
+    const std::vector<MediaType*>& get_available_media() const;
+    
+    /**
+     * @brief добавляет актёра в список исполнителей
+     * @param actor имя актёра
+     */
+    void add_actor(const std::string& actor);
+    
+    /**
+     * @brief добавляет носитель в список доступных носителей
+     * @param media указатель на объект MediaType
+     */
+    void add_media(MediaType* media);
+    
+    /**
+     * @brief перегрузка оператора вывода
+     * @param os ссылка на поток вывода
+     * @param film ссылка на объект класса Film
+     * @return ссылка на итоговый поток вывода
+     */
+    friend std::ostream& operator<<(std::ostream& os, const Film& film);
+    
+    /**
+     * @brief перегрузка оператора ввода
+     * @param is ссылка на поток ввода
+     * @param film ссылка на объект класса Film
+     * @return ссылка на итоговый поток ввода
+     */
+    friend std::istream& operator>>(std::istream& is, Film& film);
+};
 
-    public:
-        /**
-         * @brief Виртуальный деструктор
-         */
-        virtual ~Film() = default;
-
-        /**
-         * @brief Конструктор с параметрами
-         * @param title Название фильма
-         * @param director Режиссёр
-         * @param description Краткое описание
-         * @param price Цена
-         */
-        Film(const std::string& title, const Person& director, const std::string& description, double price);
-
-        /**
-         * @brief Получить название фильма
-         * @return Название
-         */
-        std::string getTitle() const;
-
-        /**
-         * @brief Получить режиссёра
-         * @return Режиссёр
-         */
-        Person getDirector() const;
-
-        /**
-         * @brief Получить описание
-         * @return Описание
-         */
-        std::string getDescription() const;
-
-        /**
-         * @brief Получить цену
-         * @return Цена
-         */
-        double getPrice() const;
-
-        /**
-         * @brief Получить список жанров
-         * @return Вектор жанров
-         */
-        std::vector<Genre> getGenres() const;
-
-        /**
-         * @brief Получить актёрский состав
-         * @return Вектор актёров
-         */
-        std::vector<Person> getCast() const;
-
-        /**
-         * @brief Получить список продаж
-         * @return Вектор продаж
-         */
-        std::vector<Sale> getSales() const;
-
-        /**
-         * @brief Добавить жанр
-         * @param genre Жанр
-         */
-        void addGenre(const Genre& genre);
-
-        /**
-         * @brief Добавить актёра
-         * @param actor Актёр
-         */
-        void addActor(const Person& actor);
-
-        /**
-         * @brief Добавить продажу
-         * @param sale Информация о продаже
-         */
-        void addSale(const Sale& sale);
-
-        /**
-         * @brief Получить общее количество проданных копий
-         * @return Общее количество продаж
-         */
-        int getTotalSales() const;
-
-        /**
-         * @brief Получить общую сумму продаж
-         * @return Сумма продаж
-         */
-        double getTotalRevenue() const;
-
-        /**
-         * @brief Проверить, есть ли указанный жанр
-         * @param genre Жанр для проверки
-         * @return true если жанр присутствует
-         */
-        bool hasGenre(const Genre& genre) const;
-
-        /**
-         * @brief Проверить, есть ли указанный актёр
-         * @param actor Актёр для проверки
-         * @return true если актёр присутствует
-         */
-        bool hasActor(const Person& actor) const;
-
-        /**
-         * @brief Получить тип носителя (виртуальная функция)
-         * @return Строка с типом носителя
-         */
-        virtual std::string getMediaType() const = 0;
-
-        /**
-         * @brief Получить полную информацию о фильме
-         * @return Строка с информацией
-         */
-        virtual std::string getInfo() const;
-    };
 }
